8842289fe8988cc1432b5daa9bb3dc0d8c875f13
tejaswinivb11 <108164022+tejaswinivb11@users.noreply.github.com>
2022-11-11 09:46:02 +0530

    first commite in "gittryjava" class second commite and push in "gittryjava" class

--- src/gitTryjava.java
+++ src/gitTryjava.java
@@ -1,5 +1,6 @@
 public class gitTryjava {
     public static void main(String[] args) {
         System.out.println("hello its been a hell");
+        System.out.println("its a second push");
     }
 }
